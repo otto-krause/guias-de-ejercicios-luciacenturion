bd4c47333cbc0f6a79464748033d07ef75f55fe4
luciacenturion <centurionluciayael@gmail.com>
2020-05-12 16:19:47 -0300

    Ejercicio 2 realizado

--- Guia 1/E2/Program.cs
+++ Guia 1/E2/Program.cs
@@ -6,7 +6,27 @@ namespace E2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             int num = 0;
+            int num2 = 0;
+            int multi = 0;
+            int divi = 0;
+            Console.WriteLine("ingrese un numero: ");
+            num = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese otro numero: ");
+            num2 = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < num2; i++)
+            {
+                multi=multi+num;                
+            }
+            Console.WriteLine("el producto es: "+multi);
+            while(num>=num2)
+            {
+                num=num-num2;
+                divi++;
+            }
+            Console.WriteLine("la division es: "+divi);
+            Console.WriteLine("el cosciente es: "+num);
         }
     }
 }
+
--- Guia 1/E3/Program.cs
+++ Guia 1/E3/Program.cs
@@ -4,9 +4,40 @@ namespace E3
 {
     class Program
     {
+        void par(int num)
+        {
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("El numero es par");
+            }
+            else
+            {
+                Console.WriteLine("El numero no es par");
+            }
+        }
+        void primo(int num)
+        {
+            int cont=0;
+            for (int i=1; i<=num; i++)
+            {
+                if(num%i==0)
+                {
+                    cont++;
+                }
+            }
+            if(cont<2)
+            {
+                Console.WriteLine("el numero es primo");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num=0;
+            Console.WriteLine("ingrese un numero");
+            num=Int32.Parse(Console.ReadLine());
+            par(num);
+            primo(num);
         }
+
     }
 }
