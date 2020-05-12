58bfb8b8da1ccf9c0282fa2929d6bf568fc2572f
luciacenturion <centurionluciayael@gmail.com>
2020-05-12 16:08:52 -0300

    Ejercicio 1 realizado

--- Guia 1/E1/Program.cs
+++ Guia 1/E1/Program.cs
@@ -6,7 +6,48 @@ namespace E1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vector = new int[10];
+            int suma = 0;
+            int menor = 0;
+            int aux = 0;
+            float prom = 0F;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ingrese un numero: ");
+                vector[i] = Int32.Parse(Console.ReadLine());     
+                suma += vector[i];
+                if(i==0)
+                {
+                    menor = vector[i];
+                }
+                else
+                {
+                    if(menor>vector[i])
+                    {
+                        menor=vector[i];
+                    }
+                }
+            }
+            Console.WriteLine("la suma es : "+suma);
+            prom = suma/10;
+            Console.WriteLine("el promedio es : "+prom);
+            for (int i = 0; i < 10; i++)
+            {
+               for(int j = i+1; j < 10; j++)
+		        {
+			        if(vector[i]<vector[j])
+			        {
+				        aux=vector[i];
+				        vector[i]=vector[j];
+				        vector[j]=aux;
+		        	}
+		        }
+            }
+            Console.WriteLine("el menor es "+ menor);         
+            for(int i=0; i < 10; i++)
+	        {
+		        Console.WriteLine("mayor a menor: "+vector[i]);
+	        }
         }
     }
 }
